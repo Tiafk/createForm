648bbfa87538246161bd2de084a55b2155d02a72
Tiafk <acshurin227@mail.ru>
2024-03-19 06:22:37 +0300

    first

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Create-Client-Form",
+  publicPath:"/createForm/",
   plugins: [vue()],
   resolve: {
     alias: {
